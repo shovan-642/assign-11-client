c2096db5ec3a6dc542fb46861721d378dc0edea9
shovan-642 <shovan642@gmail.com>
2025-04-08 21:46:07 +0600

    404 page done

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import errorImage from "../assets/404.json"
+import Lottie from 'lottie-react';
 
 const Errorpage = () => {
     return (
-        <div>
-            Opps!!!!!! page not found.
+        <div className='w-9/12 mx-auto'>
+            <Lottie className='lg:h-[350px] w-full ' animationData={errorImage}></Lottie>
+           <h1 className='text-[#FF006E] text-center font-bold text-3xl my-5'> Opps!!!!!! page not found.</h1>
         </div>
     );
 };
